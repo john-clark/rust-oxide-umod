bd1875c2dbe0e26de4454b8d123b46742fa9d7d8
John Clark <john.r.clark.3@gmail.com>
2019-02-13 23:15:15 -0600

    Update install.ps1

--- powershell/install.ps1
+++ powershell/install.ps1
@@ -7,5 +7,5 @@ mkdir backups
 git clone https://github.com/john-clark/rust-oxide-umod.git
 cd .\rust-oxide-umod
 cp umod-server-vars.cfg.example umod-server-vars.cfg
-cmd.exe /c .\install-steamcmd.bat
+cmd.exe /c .\powershell\install-steamcmd.bat
 cmd.exe /c .\umod-server-start.bat
